740f72013694c6d137c86dbcdeb21f712e395a75
Reuben Ur Rahman <reubenur.rahman@hpe.com>
2022-12-24 20:31:34 +0530

    Create demo_flask_server.py

---
+++ demo_flask_server.py
@@ -0,0 +1,30 @@
+# (C) Copyright 2022 Reubenur Rahman
+
+import json
+from flask import Flask
+
+API_VERSION = '1.0.0'
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/api/status')
+def status():
+    data = {
+        "status": "OK",
+		"version": API_VERSION
+    }
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        use_reloader=True,
+        host='0.0.0.0',
+        port=8085
+    )
