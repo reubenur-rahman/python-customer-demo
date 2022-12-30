253963498e74b9f03bc1a0810e6b43a641ca4c1d
Reuben Ur Rahman <reubenur.rahman@hpe.com>
2022-12-30 22:01:32 +0530

    Update bootstrapper.sh

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-sudo chmod 777 demo-server.service
-sudo ln -s demo-server.service /etc/systemd/system/demo-server.service
+sudo cp -f demo-server.service /etc/systemd/system/demo-server.service
+sudo chmod 777 /etc/systemd/system/demo-server.service
 sudo systemctl daemon-reload
 sudo systemctl restart demo-server.service
